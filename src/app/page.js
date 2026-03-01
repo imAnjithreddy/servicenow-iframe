02eeb2170e1175c449204508c14d96ca9f561df2
Anjith Reddy <anjithreddy5@gmail.com>
2026-03-01 18:57:12 +0530

    Refactor iframe for improved styling and accessibility

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,15 @@ export default function Home() {
     <div>
   text data
   </div>
-      <iframe src="https://dev208775.service-now.com/api/iframe_proxy/iframeProxy/proxy" 
-        style="width:100%; height:100vh; border:none;"></iframe>
+      <iframe
+        src="https://dev208775.service-now.com/api/iframe_proxy/iframeProxy/proxy"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="ServiceNow Portal"
+      />
     </div>
   );
 }
