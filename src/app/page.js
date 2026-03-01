d818b9c1ba6d16783ca567ef1c719e7eae14bbe4
Anjith Reddy <anjithreddy5@gmail.com>
2026-03-01 18:35:31 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ height: "100vh", margin: 0 }}>
+      <iframe
+        src="https://dev208775.service-now.com/sp"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="ServiceNow Portal"
+      />
+    </div>
+  );
+}
