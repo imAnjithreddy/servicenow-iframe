e3d5684c1f178936b8084bc44a672cc457aae281
Anjith Reddy <anjithreddy5@gmail.com>
2026-03-01 19:03:22 +0530

    Remove commented-out iframe from page.js
    Removed commented-out iframe code for ServiceNow Portal.

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,6 @@ export default function Home() {
   text data
   </div>
   <a href="https://dev208775.service-now.com/sp" target="_blank" rel="noopener noreferrer">Open Service Portal</a>
-     <!-- <iframe
-        src="https://dev208775.service-now.com/api/1056856/iframeproxy/proxy"
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "none",
-        }}
-        title="ServiceNow Portal"
-      /> -->
     </div>
   );
 }
